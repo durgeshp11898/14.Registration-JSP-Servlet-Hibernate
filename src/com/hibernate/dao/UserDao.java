f40af59af1de1707de358a1a8154d5eb5242d8c5
Durgesh Pawar <durgeshpawar077@gmail.com>
2021-03-20 18:57:59 -0700

    Initial commit

---
+++ src/com/hibernate/dao/UserDao.java
@@ -0,0 +1,29 @@
+package com.hibernate.dao;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.hibernate.model.User;
+import com.hibernate.util.HibernateUtil;
+
+
+public class UserDao {
+
+	public void saveUser(User user) {
+		Transaction transaction = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			// start a transaction
+			transaction = session.beginTransaction();
+			// save the student object
+			session.save(user);
+			// commit transaction
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			e.printStackTrace();
+		}
+	}
+
+}
